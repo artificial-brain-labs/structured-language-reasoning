260910f748e6dd27c1fecb00661d105b38540e92
artificial-brain-labs <purnendu@artificialbrainlabs.com>
2026-09-16 02:16:10 +0530

    Resolve named and concept entities consistently in queries

--- src/query.py
+++ src/query.py
@@ -3,23 +3,34 @@ class QueryEngine:
         self.memory = memory
         self.lexicon = lexicon
 
+    def _resolve(self, word):
+        concept = self.lexicon.concept(word)
+        if concept:
+            entity = self.memory.find_entity(concept)
+            return entity
+        return self.memory.find_named_entity(word)
+
     def answer(self, parsed):
         if parsed.question_type == "OBJECT":
-            subject_concept = self.lexicon.concept(parsed.subject_word)
+            subject = self._resolve(parsed.subject_word)
             predicate = self.lexicon.concept(parsed.verb_word)
-            if not subject_concept or not predicate:
+            if not subject or not predicate:
                 return []
-            subject = self.memory.find_entity(subject_concept)
-            return [m.object for m in self.memory.query(subject=subject, predicate=predicate)
-                    if m.status != "CONFLICTED"]
+            return [
+                m.object
+                for m in self.memory.query(subject=subject, predicate=predicate)
+                if m.status != "CONFLICTED"
+            ]
 
         if parsed.question_type == "SUBJECT":
-            object_concept = self.lexicon.concept(parsed.object_word)
+            object_id = self._resolve(parsed.object_word)
             predicate = self.lexicon.concept(parsed.verb_word)
-            if not object_concept or not predicate:
+            if not object_id or not predicate:
                 return []
-            object_id = self.memory.find_entity(object_concept)
-            return [m.subject for m in self.memory.query(predicate=predicate, object_=object_id)
-                    if m.status != "CONFLICTED"]
+            return [
+                m.subject
+                for m in self.memory.query(predicate=predicate, object_=object_id)
+                if m.status != "CONFLICTED"
+            ]
 
         return []
